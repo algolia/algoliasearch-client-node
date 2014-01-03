8f3b158564fdb66921afd568e632406b08abfab9
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-03 12:32:47 +0100

    Fixed tests

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var should = require('should'),
  */
 describe('Algolia', function () {
   // we don't have to use real hosts or API keys since we simulate the server
-  var hosts = ['YourHostname-1.algolia.io', 'YourHostname-2.algolia.io', 'YourHostname-3.algolia.io'],
+  var hosts = ['ApplicationID-1.algolia.io', 'ApplicationID-2.algolia.io', 'ApplicationID-3.algolia.io'],
       client = new Algolia('ApplicationID', 'API-Key', hosts);
 
   it('should send search request', function (done) {
@@ -20,9 +20,7 @@ describe('Algolia', function () {
       options.should.have.property('method', 'GET');
       options.should.have.property('hostname');
       hosts.should.include(options.hostname);
-
-      // Wait, this is weird, the spce should be escaped. I'll open an issue
-      options.should.have.property('path', '/1/indexes/cities?query=loz anqel');
+      options.should.have.property('path', '/1/indexes/cities?query=loz%20anqel');
 
       return {
         statusCode: 200,
@@ -31,8 +29,8 @@ describe('Algolia', function () {
     });
 
     var index = client.initIndex('cities');
-    index.search('loz anqel', function(success, content) {
-      success.should.eql(true);
+    index.search('loz anqel', function(error, content) {
+      error.should.eql(false);
       content.should.have.property('hits').with.lengthOf(1);
       content.hits[0].should.have.property('name', 'Los Angeles');
       content.hits[0].should.have.property('_highlightResult');
@@ -44,16 +42,16 @@ describe('Algolia', function () {
   it('should send search request with 1 server down', function (done) {
     // we make the first request fail, so that the driver will try a second server
     mockHttps.setResponders([{
-      statusCode: 500,
-      json: null
+      statusCode: 503,
+      json: { message: "Fail 1" }
     }, {
       statusCode: 200,
       json: require('./responses/search1.json')
     }]);
 
     var index = client.initIndex('cities');
-    index.search('loz anqel', function(success, content) {
-      success.should.eql(true);
+    index.search('loz anqel', function(error, content) {
+      error.should.eql(false);
       content.should.have.property('hits').with.lengthOf(1);
       content.hits[0].should.have.property('name', 'Los Angeles');
       content.hits[0].should.have.property('_highlightResult');
@@ -65,19 +63,19 @@ describe('Algolia', function () {
     it('should send search request with all servers down', function (done) {
     // we make the first request fail, so that the driver will try a second server
     mockHttps.setResponders([{
-      statusCode: 500,
-      json: null
+      statusCode: 503,
+      json: { message: "Fail 1" }
     }, {
-      statusCode: 500,
-      json: null
+      statusCode: 503,
+      json: { message: "Fail 2" }
     }, {
-      statusCode: 500,
-      json: null
+      statusCode: 503,
+      json: { message: "Fail 3" }
     }]);
 
     var index = client.initIndex('cities');
-    index.search('loz anqel', function(success, content) {
-      success.should.eql(false);
+    index.search('loz anqel', function(error, content) {
+      error.should.eql(true);
       done();
     });
   });  
