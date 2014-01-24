223d272304f8bea6fb074f7476d421cea8e7c9fd
Xavier Grand <grand.xavier@gmail.com>
2014-01-23 18:50:14 -0800

    Fixed some functions

--- algoliasearch-node.js
+++ algoliasearch-node.js
@@ -172,7 +172,7 @@ AlgoliaSearch.prototype = {
     addUserKey: function(acls, callback) {
         var aclsObject = {};
         aclsObject.acl = acls;
-        this._request('POST', '/1/keys', aclsObjects, callback);
+        this._request('POST', '/1/keys', aclsObject, callback);
     },
     /*
      * Add an existing user key
@@ -405,7 +405,7 @@ AlgoliaSearch.prototype.Index.prototype = {
          *  content: the server answer that updateAt and taskID
          */
         addObjects: function(objects, callback) {
-            this._batch(objects, 'addObjects', callback);
+            this._batch(objects, 'addObject', callback);
         },
         /*
          * Get an object from this index
@@ -428,7 +428,7 @@ AlgoliaSearch.prototype.Index.prototype = {
                     params += attributes[i];
                 }
             }
-            this.as._request('GET', '/1/indexes/' + encodeURIComponent(this.indexName) + '/' + encodeURIComponent(objectID) + params, postObj, callback);
+            this.as._request('GET', '/1/indexes/' + encodeURIComponent(this.indexName) + '/' + encodeURIComponent(objectID) + params, objectID, callback);
         },
 
         /*
@@ -476,8 +476,14 @@ AlgoliaSearch.prototype.Index.prototype = {
          *  content: the server answer that updateAt and taskID
          */
         saveObjects: function(objects, callback) {
-            this._batch(objects, 'updateObjects', callback);
+            this._batch(objects, 'updateObject', callback);
         },
+
+
+        batch: function(request, callback) {
+            this.as._request('POST', '/1/indexes/' + encodeURIComponent(this.indexName) + '/batch', request, callback);
+        },
+
         /*
          * Delete an object from the index
          *
