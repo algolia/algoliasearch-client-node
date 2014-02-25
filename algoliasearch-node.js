5179d854e6d79fdab1e18ed632c912762da1c8dd
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-25 14:40:00 -0800

    Bump to 1.5.5

--- algoliasearch-node.js
+++ algoliasearch-node.js
@@ -359,7 +359,7 @@ AlgoliaSearch.prototype = {
             'X-Algolia-API-Key': this.apiKey,
             'Connection':'keep-alive',
             'Content-Length': 0,
-            'User-Agent': 'Algolia for node.js'
+            'User-Agent': 'Algolia for node.js 1.5.5'
           }
         };
         if (this.forwardAdminAPIKey !== null) {
