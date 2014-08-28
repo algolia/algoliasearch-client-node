b9589784e21e92d7f50de1ed5f939e54866816c9
Xavier Grand <grand.xavier@gmail.com>
2014-08-28 11:32:56 +0200

    Bump to 1.5.11

--- algoliasearch-node.js
+++ algoliasearch-node.js
@@ -60,7 +60,7 @@ var AlgoliaSearch = function(applicationID, apiKey, httpsAgent, hostsArray) {
 /**
  * Version
  */
-AlgoliaSearch.version = '1.5.10';
+AlgoliaSearch.version = '1.5.11';
 
 AlgoliaSearch.prototype = {
     /*
